cd8d0975bff833c5c12f4da94e692e49c98c02a8
diemtran2806 <ttdiem2002@gmail.com>
2023-04-14 16:56:18 +0700

    add login component 🎉

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,71 @@
 import Card from "../components/Card";
+import { useState } from "react";
 const LoginPage = () => {
+  const [errors, setErrors] = useState({});
+  const [form, setForm] = useState({});
+
+  const onChange = ({ name, value }) => {
+    setForm({ ...form, [name]: value });
+
+    if (value !== "") {
+      if (name === "password") {
+        if (value.length < 6) {
+          setErrors((prev) => {
+            return {
+              ...prev,
+              [name]: "Password need min 6 characters!",
+            };
+          });
+        } else {
+          setErrors((prev) => {
+            return { ...prev, [name]: null };
+          });
+        }
+      }
+      if (name === "username") {
+        setErrors((prev) => {
+          return { ...prev, [name]: null };
+        });
+      }
+    } else {
+      setErrors((prev) => {
+        return { ...prev, [name]: "This field is required!" };
+      });
+    }
+  };
+
+  const onSubmit = () => {
+    if (!form.username) {
+      setErrors((prev) => {
+        return { ...prev, username: "Please add a username!" };
+      });
+    }
+    if (!form.password) {
+      setErrors((prev) => {
+        return { ...prev, password: "Please add a password!" };
+      });
+    }
+
+    if (
+      Object.values(form).length === 2 &&
+      Object.values(form).every((item) => item.trim().length > 0) &&
+      Object.values(errors).every((item) => !item)
+    ) {
+      // register(form)(authDispatch)((response) => {
+      //   navigate(LOGIN, {data: response});
+      // });
+      // const newForm = {
+      //   email: email,
+      //   username: form.username,
+      //   password: form.password,
+      // };
+      // register(newForm)(authDispatch);
+      // navigation.navigate('Login');
+    }
+  };
+
   return (
-    <div>
-      Hello
-      <Card />
-    </div>
+    <Card onSubmit={onSubmit} onChange={onChange} form={form} errors={errors} />
   );
 };
 
