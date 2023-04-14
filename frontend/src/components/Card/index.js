61a3c06ed149b721880caf88c9a450ba4f3cf6b6
diemtran2806 <ttdiem2002@gmail.com>
2023-04-14 16:56:18 +0700

    add login component 🎉

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -1,11 +1,37 @@
 import classes from "./Card.module.css";
-const Card = () => {
+import Input from "../Input";
+const Card = ({ onChange, onSubmit, form, errors }) => {
   return (
     <div className={`${classes.wapperLogin} ${classes.sizeWrapper}`}>
-      <div>
+      <div className={`${classes.container} ${classes.login}`}>
         <header className={classes.modalHeader}>Đăng Nhập</header>
-        <div class="modal-body">
-          <div class="modal-form"></div>
+        <div className={classes.body}>
+          <div className={classes.form}>
+            <form method="post">
+              <Input
+                label="Tài khoản"
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Nhập tên tài khoản"
+                onChange={(event) => {
+                  onChange({ name: "username", value: event.target.value });
+                }}
+                error={errors.username}
+              />
+              <Input
+                label="Mật khẩu"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Nhập mật khẩu"
+                onChange={(event) => {
+                  onChange({ name: "password", value: event.target.value });
+                }}
+                error={errors.password}
+              />
+            </form>
+          </div>
         </div>
       </div>
     </div>
