918d2f2cc2446f62a7cc4686d5835e8b60331e26
levandat <lvd.levandat@gmail.com>
2023-04-15 17:59:28 +0700

    Create Faculty page 🏚

--- frontend/src/pages/Faculty.js
+++ frontend/src/pages/Faculty.js
@@ -1,5 +1,31 @@
-const FacultyPage = () => {
-  return <div>Faculty ne</div>;
+import React from "react";
+import { useState, useEffect } from "react";
+import TableList from "../components/ListTable";
+
+const FacultyPage = (props) => {
+  const Faculty = [
+    {id: 1, "Tên Khoa":"Khoa CNTT"},
+    {id: 2, "Tên Khoa":"Khoa QLDA"},
+    {id: 3, "Tên Khoa":"Khoa Điện"},
+    {id: 4, "Tên Khoa":"Khoa Kiến trúc"},
+  ]
+
+  const [isAdmin, setIsAdmin] = useState(props.admin);
+  
+  useEffect(() => {
+    setIsAdmin(props.admin);
+  }, [props.admin]);
+
+  return (
+    <>
+      {
+        isAdmin?
+          <TableList key="admin" data={Faculty} update={true} detail={true}/>
+        :
+          <TableList key="user" data={Faculty} detail={true}/>
+      }
+    </>
+  );
 };
 
 export default FacultyPage;
--- frontend/src/pages/Student.js
+++ frontend/src/pages/Student.js
@@ -3,10 +3,10 @@ import { useState } from "react";
 import TableList from "../components/ListTable";
 const StudentsPage = (props) => {
   const students = [
-    { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com',hihi: 'wasif@email.com'},
-    { id: 6, name: 'Ali', age: 19, email: 'ali@email.com',hihi: 'wasif@email.com'},
-    { id: 3, name: 'Saad', age: 16, email: 'saad@email.com',hihi: 'wasif@email.com'},
-    { id: 4, name: 'Asad', age: 25, email: 'asad@email.com',hihi: 'wasif@email.com'},
+    { id: 1, Tên: 'Wasif', Tuổi: 21, email: 'wasif@email.com',hihi: 'wasif@email.com'},
+    { id: 6, Tên: 'Ali', Tuổi: 19, email: 'ali@email.com',hihi: 'wasif@email.com'},
+    { id: 3, Tên: 'Saad', Tuổi: 16, email: 'saad@email.com',hihi: 'wasif@email.com'},
+    { id: 4, Tên: 'Asad', Tuổi: 25, email: 'asad@email.com',hihi: 'wasif@email.com'},
   ]
   
   const [isAdmin, setIsAdmin] = useState(props.admin);
