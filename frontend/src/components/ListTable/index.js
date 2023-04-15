4138cefbc10682a19652250f3629cc95d5bf5693
levandat <lvd.levandat@gmail.com>
2023-04-15 20:04:11 +0700

    call apis get data Student

--- frontend/src/components/ListTable/index.js
+++ frontend/src/components/ListTable/index.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import tableStyle from "./ListTable.module.css";
 import { HiPencilSquare } from "react-icons/hi2";
 import { RiDeleteBinFill } from "react-icons/ri";
 import { FaInfoCircle } from "react-icons/fa";
 
 const TableList = (props) => {
-  const [datas, setDatas] = useState(props.data);
+  const [data, setData] = useState(props.data);
+  useEffect(() => {
+    setData(props.data);
+  }, [props.data]);
+
   const [checked, setChecked] = useState([]);
   const [isCheckAll, setIsCheckAll] = useState(false);
-  const [header,setHeader] = useState(Object.keys(datas[0]).map((key, index) => key));
+  const [header,setHeader] = useState(Object.keys(data[0]).map((key, index) => key));
   const [update, setUpdate] = useState(props.update);
   const [del, setDel] = useState(props.del);
   const [checkbox,setCheckbox] = useState(props.checkbox)
   const [detail,setDetail] = useState(props.detail)
-
 
   const handleCheck = (event) => {
     const checkboxValue = parseInt(event.target.value);
@@ -28,7 +31,7 @@ const TableList = (props) => {
   const handleCheckAll = (event) => {
     const checkboxValue = parseInt(event.target.value);
     if (event.target.checked) {
-      const allCheckboxValues = datas.map((data) => data.id);
+      const allCheckboxValues = data.map((data) => data.id);
       setChecked(allCheckboxValues);
     } else {
       setChecked([]);
@@ -54,8 +57,8 @@ const TableList = (props) => {
             <tr>
               {
                 checkbox?
-                  <th>
-                    <input type="checkbox" checked={datas.length === checked.length} onChange={handleCheckAll}/>
+                  <th >
+                    <input type="checkbox" checked={data.length === checked.length} onChange={handleCheckAll}/>
                   </th>
                 :
                   <></>
@@ -68,7 +71,7 @@ const TableList = (props) => {
                 header.map((key, index) => {
                     if(key !== 'id'){
                       return(
-                        <th>{key.toUpperCase()}</th>
+                        <th key={index}>{key.toUpperCase()}</th>
                       )
                     }
                   }
@@ -94,63 +97,67 @@ const TableList = (props) => {
 
   const renderTableData = () => {
     return(
-        datas.map((data, index) => {
-          return (
-              <tr key={data.id}>
-                    
-                {
-                  checkbox?
-                    <td className={tableStyle.textcenter}>
-                      <input value={data.id} type="checkbox" onChange={handleCheck} checked={checked.includes(data.id)}/>
-                    </td> 
-                  :
-                  <></>
-                }
-
-                <td td className={tableStyle.textcenter}>
-                  {index}
-                </td> 
-
-                {
-                  header.map((key, index)=>{
-                    if(key !== 'id'){
-                      return(
-                        <td>
-                          {
-                            data[key]
-                          }
-                        </td>
-                      )
+        <tbody>
+          {
+            data.map((data, index) => {
+              return (
+                  <tr key={data.id}>
+                        
+                    {
+                      checkbox?
+                        <td className={tableStyle.textcenter}>
+                          <input value={data.id} type="checkbox" onChange={handleCheck} checked={checked.includes(data.id)}/>
+                        </td> 
+                      :
+                      <></>
                     }
-                  })
-                }
-
-                {
-                  update?
+    
                     <td className={tableStyle.textcenter}>
-                      <button onClick={()=>handleUpdate(data.id)}><HiPencilSquare/></button>
-                    </td>
-                  :<></>
-                }
-
-                {
-                  del?
-                    <td className={tableStyle.textcenter}>
-                      <button onClick={()=>handleDelete(data.id)}><RiDeleteBinFill/></button>
-                    </td>
-                  :<></>
-                }
-
-                {
-                  detail?
-                    <td className={tableStyle.textcenter}>
-                      <button onClick={()=>handleDetail(data.id)}><FaInfoCircle/></button>
-                    </td>
-                  :<></>
-                }
-              </tr>
-          )
-        })
+                      {index}
+                    </td> 
+    
+                    {
+                      header.map((key, index)=>{
+                        if(key !== 'id'){
+                          return(
+                            <td key={index}>
+                              {
+                                data[key]
+                              }
+                            </td>
+                          )
+                        }
+                      })
+                    }
+    
+                    {
+                      update?
+                        <td className={tableStyle.textcenter}>
+                          <button onClick={()=>handleUpdate(data.id)}><HiPencilSquare/></button>
+                        </td>
+                      :<></>
+                    }
+    
+                    {
+                      del?
+                        <td className={tableStyle.textcenter}>
+                          <button onClick={()=>handleDelete(data.id)}><RiDeleteBinFill/></button>
+                        </td>
+                      :<></>
+                    }
+    
+                    {
+                      detail?
+                        <td className={tableStyle.textcenter}>
+                          <button onClick={()=>handleDetail(data.id)}><FaInfoCircle/></button>
+                        </td>
+                      :<></>
+                    }
+                  </tr>
+              )
+            })
+          }
+        </tbody>
     )
   };
 
--- frontend/src/pages/Student.js
+++ frontend/src/pages/Student.js
@@ -1,22 +1,62 @@
 import React from "react";
-import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import TableList from "../components/ListTable";
 const StudentsPage = (props) => {
-  const students = [
-    { id: 1, Tên: 'Wasif', Tuổi: 21, email: 'wasif@email.com',hihi: 'wasif@email.com'},
-    { id: 6, Tên: 'Ali', Tuổi: 19, email: 'ali@email.com',hihi: 'wasif@email.com'},
-    { id: 3, Tên: 'Saad', Tuổi: 16, email: 'saad@email.com',hihi: 'wasif@email.com'},
-    { id: 4, Tên: 'Asad', Tuổi: 25, email: 'asad@email.com',hihi: 'wasif@email.com'},
-  ]
-  
+  // const students = [
+  //   { id: 1, Tên: 'Wasif', Tuổi: 24, email: 'wasif@email.com',hihi: 'wasif@email.com'},
+  //   { id: 6, Tên: 'Ali', Tuổi: 19, email: 'ali@email.com',hihi: 'wasif@email.com'},
+  //   { id: 3, Tên: 'Saad', Tuổi: 16, email: 'saad@email.com',hihi: 'wasif@email.com'},
+  //   { id: 4, Tên: 'Asad', Tuổi: 25, email: 'asad@email.com',hihi: 'wasif@email.com'},
+  // ]
+  const [students, setStudents] = useState([])
+  const [loading, setLoading] = useState(true);
   const [isAdmin, setIsAdmin] = useState(props.admin);
+  useEffect(() => {
+    setIsAdmin(props.admin);
+  }, [props.admin]);
 
+  const [searchParams] = useSearchParams();
+  const [id, setId] = useState(Number(searchParams.get("id")) || 1);
+
+  useEffect(() => {
+     if (id) {
+      // lấy data sinh viên theo lớp
+      axios.get(`https://reqres.in/api/users?page=${id}`)
+        .then(response => {
+          console.log(response.data.data)
+          setStudents(response.data.data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.log(error);
+          setLoading(false);
+        });
+    } else {
+      // nếu không có thì lấy tất cả sinh viên 
+      axios.get("https://reqres.in/api/users")
+        .then(response => {
+          setStudents(response.data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.log(error);
+          setLoading(false);
+        });
+    }
+  }, [id]);
+  
   return <>
         {
-          isAdmin?
-            <TableList data={students} update={true} del={true} checkbox={true}/>
-          :
-            <TableList data={students}/>
+          loading ? (
+            <p>Loading..{id}</p>
+          ):
+          (
+            isAdmin?
+              <TableList key="admin" data={students} update={true} del={true} checkbox={true}/>:
+              <TableList key="user" data={students}/>
+          )
         }
   </>;
 };
