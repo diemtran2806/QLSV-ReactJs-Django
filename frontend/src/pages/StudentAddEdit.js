ed64fc8996bfe1af594d5f5d815be24e9eb492ab
levandat <lvd.levandat@gmail.com>
2023-05-10 01:50:27 +0700

    call api to update and create

--- frontend/src/pages/StudentAddEdit.js
+++ frontend/src/pages/StudentAddEdit.js
@@ -163,11 +163,7 @@ const StudentAddEdit = (props) => {
                 address:"",
                 dob:"",
                 classId:"",
-<<<<<<< HEAD
                 avatar:"https://tinyurl.com/2l59av9t"
-=======
-                avatar:"https://scontent.fhan3-1.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?stp=cp0_dst-png_p60x60&_nc_cat=1&ccb=1-7&_nc_sid=7206a8&_nc_ohc=blIOUfZoi4EAX-l182P&_nc_ht=scontent.fhan3-1.fna&oh=00_AfD-rSsRpHTWGNKgzAlsN1Djrz-oyfuo5KVY1Qng3C-LQw&oe=646FE778"
->>>>>>> f661503b5bf511809b9a67fe96619bd70aa6a90a
             }
             setFormValue(form);
             setLoading(false);
