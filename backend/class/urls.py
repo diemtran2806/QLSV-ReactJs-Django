34ea1c94fff31b50fb3bde25b0ac413c6eefc537
MSI\vieta <vietanha29@gmail.com>
2023-05-17 14:22:56 +0700

    Tuna////

--- backend/class/urls.py
+++ backend/class/urls.py
@@ -3,9 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', classes_view),
-    path('/<int:id>/', class_view),
+    path('/<int:id>', class_view),
     path('/create', class_create_view),
-    path('/<int:id>/update/', class_update_view),
-    path('/<int:id>/delete/', class_delete_view),
-    path('/<int:id>/faculty/', class_view_by_faculty),
+    path('/<int:id>/update', class_update_view),
+    path('/<int:id>/delete', class_delete_view),
+    path('/<int:id>/faculty', class_view_by_faculty),
 ]
