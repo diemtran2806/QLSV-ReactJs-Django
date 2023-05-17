7c65b8a55454815561cad3b06656433e38d22803
MSI\vieta <vietanha29@gmail.com>
2023-05-17 14:29:24 +0700

    TuNa/Remove-Admin

--- backend/lecturer/models.py
+++ backend/lecturer/models.py
@@ -3,5 +3,4 @@ from django.db import models
 
 class Lecturer(models.Model):
     id_user = models.ForeignKey('users.Users', on_delete=models.CASCADE)
-    is_admin = models.BooleanField()
     id_faculty = models.ForeignKey('faculty.Faculty', on_delete=models.CASCADE)
